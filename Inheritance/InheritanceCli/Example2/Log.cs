076278ba68497605fe007ea0562947d4e8a9d461
Goossens Levi <Levi.Goossens@student.pxl.be>
2025-04-22 09:04:40 +0200

    Make Log class

---
+++ Inheritance/InheritanceCli/Example2/Log.cs
@@ -0,0 +1,27 @@
+﻿namespace InheritanceCli.Example2
+{
+    class Log
+    {
+        private Dictionary<DateTime, string> _log = new Dictionary<DateTime, string>();
+
+        /// <summary>
+        /// Voegt een entry toe aan de Dictionary _log
+        /// </summary>
+        /// <param name="message"></param>
+        public void Write(string message)
+        {
+            _log.Add(DateTime.Now, message);
+        }
+
+        /// <summary>
+        /// Toont de volledige log(Dictionary)
+        /// </summary>
+        public void DisplayLog()
+        {
+            foreach (var item in _log)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
