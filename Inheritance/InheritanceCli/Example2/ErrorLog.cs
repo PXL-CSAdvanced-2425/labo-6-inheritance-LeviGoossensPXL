6ba1ff4c3d5d70be64ba8edf2b02dcfa9604d78e
Goossens Levi <Levi.Goossens@student.pxl.be>
2025-04-22 09:12:00 +0200

    make ErrorLog class

---
+++ Inheritance/InheritanceCli/Example2/ErrorLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceCli.Example2
+{
+    class ErrorLog : Log
+    {
+        public override void Write(string message)
+        {
+            base.Write($"ERROR {message}");
+        }
+    }
+}
